07b453ba12addd5943f64977ce410b458143de20
pauloalchemist <paulolinsdev@gmail.com>
2022-02-05 19:48:14 -0300

    extracting the argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    let (query, filename) = parse_config(&args);
+
+    println!("Pesquisando por {} no arquivo {}.", query, filename);
+
+    println!("No arquivo {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Algo errado na leitura do arquivo.");
+
+    println!("Existe o conteúdo:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
     let query = &args[1];
     let filename = &args[2];
-    println!("Pesquisando por {} no arquivo {}.", query, filename);
+
+    (query, filename)
 }
