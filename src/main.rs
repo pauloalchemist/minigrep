75aeb60aba39e15b25d13dc27894f6a74a4cd928
pauloalchemist <paulolinsdev@gmail.com>
2022-02-06 14:49:57 -0300

    splitting code into a library crate

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use minigrep::{run, Config};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Ocorreu um problema com os argumentos: {}", err);
+        println!("Ocorreu um problema: {}", err);
         process::exit(1)
     });
 
@@ -18,31 +18,8 @@ fn main() {
 
     println!("No arquivo {}", config.filename);
 
-    run(config);
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("Existe o conteúdo:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Argumentos insuficientes para realizar a operação.");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+    if let Err(e) = run(config) {
+        println!("Erro na aplicação: {}", e);
+        process::exit(1);
     }
 }
